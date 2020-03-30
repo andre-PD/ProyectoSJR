c07c16c765186bc660a7a5fa85c811519ca2c96f
Angel Lucero <Angel Lucero@DESKTOP-IUT2LV0>
2020-03-30 18:10:18 -0500

    INFO

--- ProyectoSJR/src/modelo/PORTI.java
+++ ProyectoSJR/src/modelo/PORTI.java
@@ -12,5 +12,12 @@ package modelo;
 public class PORTI {
     public static void main(String[] args) {
         System.out.println(" - -   H O L A   C O R A Z Ó N   - - ");
+        
+        /*
+        ADD: PARA AÑADIR ARCHIVOS A CARGAR
+        COMMIT: INFORMACIÓN DE LOS CAMBIOS O AVANVES DEL PROYECTO (SE HACE EN TODOS LOS PUSH)
+        PUSH: PARA SUBIR LOS CAMBIOS O AVANVES DEL PROYECTO AL REPOSITORIO
+        PULL: DESCARGAR LOS CAMBIOS QUE OTROS USUARIOS HICIERON EN EL PROYECTO
+        */
     }
 }
